80590c82a1b031ce803c10ec042866ae26586e71
Yongtae Park <samuel.park@mz.co.kr>
2025-12-12 06:30:29 +0900

    feat(app): add react-query with QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ModalProvider } from "./shared/modal/ModalContext.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { globalQueryClient } from "./app/query/query-client.ts";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ModalProvider>
-      <App />
-    </ModalProvider>
+    <QueryClientProvider client={globalQueryClient}>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
