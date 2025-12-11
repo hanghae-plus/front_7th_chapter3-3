a53c4dad6e76a0f4f8e801facc875bbfcdaafa1b
Yongtae Park <samuel.park@mz.co.kr>
2025-12-12 07:10:56 +0900

    feat(shared): add useUrlSearchParams hook

---
+++ src/shared/hooks/use-url-search-params.ts
@@ -0,0 +1,13 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+export function useUrlSearchParams() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const handleUpdateQueryParams = (key: string, value: string) => {
+    queryParams.set(key, value);
+    navigate(`?${queryParams.toString()}`);
+  };
+  return { queryParams, handleUpdateQueryParams };
+}
