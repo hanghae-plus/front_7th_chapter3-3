33e53503ed40ce5216fd17bbed8b7a6a143b7541
seunghoonKang <tmdgns4321@gmail.com>
2025-12-12 04:56:03 +0900

    feat - Context Providers 통합 (PostProvider, CommentProvider, UserProvider)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router } from "react-router-dom"
 import { Header, Footer } from "./widgets"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { PostProvider } from "@/features/post"
+import { CommentProvider } from "@/features/comment"
+import { UserProvider } from "@/features/user"
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
+      <PostProvider>
+        <CommentProvider>
+          <UserProvider>
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex-grow container mx-auto px-4 py-8">
+                <PostsManagerPage />
+              </main>
+              <Footer />
+            </div>
+          </UserProvider>
+        </CommentProvider>
+      </PostProvider>
     </Router>
   )
 }
