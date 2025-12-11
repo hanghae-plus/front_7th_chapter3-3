c7414d63257bf7ee28b142e258b9d21acfdf5b68
lecto17 <khwoo_rpsit@naver.com>
2025-12-11 11:13:59 +0900

    chore: app 폴더 내로 app, main 파일 분리
    (cherry picked from commit eb434b858a8256c277c9079de2cedc5eba67b7cc)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./components/Header.tsx"
-import Footer from "./components/Footer.tsx"
+import Header from "./widgets/Header.tsx"
+import Footer from "./widgets/Footer.tsx"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
 
 const App = () => {
