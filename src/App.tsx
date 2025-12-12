059a9c3056e48f609accc2df36d27094915c9719
Yongtae Park <samuel.park@mz.co.kr>
2025-12-12 15:59:59 +0900

    refactor(widgets): move Header/Footer to widgets layer for FSD compliance

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/Header.tsx";
-import Footer from "./components/Footer.tsx";
+import { Header, Footer } from "./widgets";
 import PostsManagerPage from "./pages/posts-manager/ui/PostsManagerPage.tsx";
 import { PostsUrlQueryProvider } from "./shared/url-query";
 
